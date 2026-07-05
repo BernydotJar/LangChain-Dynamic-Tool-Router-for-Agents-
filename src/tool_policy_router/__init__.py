103a67ba6ee3a1ab85fae7197557c7808430055a
BernydotJar <fearlesed@gmail.com>
2026-07-04 19:32:19 -0600

    Export billing entitlement contracts

--- src/tool_policy_router/__init__.py
+++ src/tool_policy_router/__init__.py
@@ -2,6 +2,27 @@
 
 from .adapter import LangGraphToolRouterMiddleware, RuntimeToolInjector, fallback_tool
 from .audit import AuditEvent, InMemoryAuditLog
+from .billing import (
+    BillingConfig,
+    CheckoutRequest,
+    CheckoutSessionRequest,
+    CheckoutSessionResult,
+    Entitlement,
+    InMemoryLicenseActivationStore,
+    InMemoryWebhookEventStore,
+    LicenseActivation,
+    MockStripeGateway,
+    PortalSessionRequest,
+    PortalSessionResult,
+    SubscriptionRecord,
+    build_checkout_session_request,
+    build_webhook_event_record,
+    create_checkout_session,
+    create_portal_session,
+    entitlement_from_subscription,
+    hash_license_key,
+    hash_machine_fingerprint,
+)
 from .contracts import Principal, RouteResult, ToolDecision, ToolPolicy, ToolRequestContext
 from .policy import ToolPolicyRouter
 from .store import FileAuditStore, FilePolicyStore, PolicyBundle
@@ -9,19 +30,38 @@ from .tool_registry import CallableTool, ToolRegistry
 
 __all__ = [
     "AuditEvent",
+    "BillingConfig",
     "CallableTool",
+    "CheckoutRequest",
+    "CheckoutSessionRequest",
+    "CheckoutSessionResult",
+    "Entitlement",
     "FileAuditStore",
     "FilePolicyStore",
     "InMemoryAuditLog",
+    "InMemoryLicenseActivationStore",
+    "InMemoryWebhookEventStore",
     "LangGraphToolRouterMiddleware",
+    "LicenseActivation",
+    "MockStripeGateway",
     "PolicyBundle",
+    "PortalSessionRequest",
+    "PortalSessionResult",
     "Principal",
     "RouteResult",
     "RuntimeToolInjector",
+    "SubscriptionRecord",
     "ToolDecision",
     "ToolPolicy",
     "ToolPolicyRouter",
     "ToolRegistry",
     "ToolRequestContext",
+    "build_checkout_session_request",
+    "build_webhook_event_record",
+    "create_checkout_session",
+    "create_portal_session",
+    "entitlement_from_subscription",
     "fallback_tool",
+    "hash_license_key",
+    "hash_machine_fingerprint",
 ]
